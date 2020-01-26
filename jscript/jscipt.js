e76f3ae83cd6cfd04099950af93573fa5e6b2462
Rayton Burrell <51148872+BurRay@users.noreply.github.com>
2020-01-26 03:08:28 -0500

    Fixed issue #1

--- jscript/jscipt.js
+++ jscript/jscipt.js
@@ -1,26 +1,3 @@
-function SwitchImage() {
-    // this code is executed each time the user scrolls
-    var scrollPosition = window.pageYOffset;
-    var mylogo = document.getElementById("mylogo");
-    var myavatar = document.getElementById("avatar");
-
-
-    // scrollPosition is 0 at the top of the page
-    // it contains how many pixels have been scrolled down
-    if (scrollPosition > 1) {
-        myavatar.style.display = "none";
-        mylogo.style.display ="inline-block";
-    } 
-    else {
-        myavatar.style.display = "inline-block";
-        mylogo.style.display ="none";
-    }
-
-    //mylogo = "../media/images/bluelogo.png";
-    // mylogo = "///C:/Users/rayto/Programming%20Projects/MyWebsite/media/images/sopm";
-    //alert(mylogo);
-}
-
 // Get the navbar
 var navibar = document.getElementById("navibar");
 
@@ -36,12 +13,40 @@ function Sticknavbar()
    { navibar.classList.remove("stickynavbar");}
 }
 
-window.onscroll = function () {
-    this.SwitchImage();
-    //this.Sticknavbar();
-}
 
 //object.addEventListener ("scroll", onScrollHandler);
-////////////////////
 
+window.onscroll = function () {
+    this.SwitchToAvatar();
+}
+
+
+window.onclick=function()  
+{
+    this.SwitchToLogo();   
+};
+
+
+function SwitchToAvatar() {
+    // this code is executed each time the user scrolls
+    var scrollPosition = window.pageYOffset;
+    var mylogo = document.getElementById("mylogo");
+    var myavatar = document.getElementById("avatar");
+
+
+    // scrollPosition is 0 at the top of the page
+    // it contains how many pixels have been scrolled down
+    if (scrollPosition = 0) {
+        myavatar.style.display = "inline-block";
+        mylogo.style.display ="none";
+    } 
+}
+
+
+function SwitchToLogo()
+{
+    var myavatar = document.getElementById("avatar");
+    myavatar.style.display = "none";
+    mylogo.style.display ="inline-block";
+}
 
