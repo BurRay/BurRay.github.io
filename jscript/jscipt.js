94cc64c276252aa643ae9558a6f4a85e491c7ca0
Rayton Burrell <raytonburrell@gmail.com>
2020-10-03 10:55:59 -0500

    added framework for redesign

---
+++ jscript/jscipt.js
@@ -0,0 +1,14 @@
+var nav = document.getElementById('nav');
+var upperBound = 100;
+var lowerBound = 95;
+var navTop = parseInt(window.getComputedStyle(nav, null).marginTop);
+var navRadius = parseInt(window.getComputedStyle(nav, null).borderRadius);
+
+document.addEventListener('scroll', function (e) {
+  var tickValue = (upperBound - lowerBound) / navTop;
+  var offset = window.scrollY;
+  var widthCount = lowerBound + offset * tickValue;
+  var radiusCount = navRadius - offset * tickValue;
+  nav.style.width = widthCount + '%';
+  nav.style.borderRadius = radiusCount + 'px';
+});
