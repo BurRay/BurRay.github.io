418aa56db2bcea23fff90953a2b65ecb410d1a88
Rayton Burrell <raytonburrell@gmail.com>
2020-10-03 16:23:28 -0500

    added active section slider under navigation link

--- jscript/jscipt.js
+++ jscript/jscipt.js
@@ -14,3 +14,36 @@ document.addEventListener('scroll', function (e) {
   nav.style.borderRadius = radiusCount + 'px';
   header.style.filter = 'blur(' + offset / 100 + 'px)';
 });
+
+/////////////////////////////////  https://codepen.io/neiya/pen/VwLqQPO?editors=1010
+
+setInterval(changeOrder, 2000);
+
+function changeOrder() {
+  const allIndicators = document.querySelectorAll('.indicator');
+  const one = '1';
+  const two = '2';
+  const three = '3';
+  const four = '4';
+
+  for (const slide of allIndicators) {
+    const order = slide.getAttribute('data-order');
+
+    console.log({ order });
+
+    switch (order) {
+      case one:
+        slide.setAttribute('data-order', two);
+        break;
+      case two:
+        slide.setAttribute('data-order', three);
+        break;
+      case three:
+        slide.setAttribute('data-order', four);
+        break;
+      case four:
+        slide.setAttribute('data-order', one);
+        break;
+    }
+  }
+}
